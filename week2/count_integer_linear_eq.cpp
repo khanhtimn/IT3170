06ccebe57ac662e2a0beed46d2fe654e4a86e9bd
KhanhTIMN <khanhlcbb@gmail.com>
2025-04-17 18:23:29 +0700

    week2 - count_integer_linear_equation

--- week2/count_integer_linear_eq.cpp
+++ week2/count_integer_linear_eq.cpp
@@ -1,3 +1,27 @@
+/*
+Description
+Given a positive integer n and n positive integers a1, a2, ..., an.
+Compute the number of positive integer solutions to the equation:
+a1X1 + a2X2 + . . . + anXn = M
+
+Input
+Dòng 1: n và M
+Dòng 2: a
+1
+, a
+2
+, ..., a
+n
+Output
+   Số nghiệm nguyên dương
+Ví dụ
+Input
+3 5
+1 1 1
+Output
+6
+*/
+
 #include <iostream>
 #include <vector>
 
