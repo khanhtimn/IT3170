7b29a80a69cca95ad4aa5c602e97c5b301774a21
KhanhTIMN <khanhtimn@KhanhTIMNs-MacBook-Air.local>
2025-04-17 17:27:12 +0700

    week2 - balanced_courses_assignment

--- week2/balanced_courses_assignments.cpp
+++ week2/balanced_courses_assignments.cpp
@@ -1,3 +1,52 @@
+/*
+Description
+The department D has m teachers T={1,2,...,m} and n courses C={1,2,...,n}.
+Input
+The input consists of following lines
+Line 1: contains two integer m and n (1≤m≤10, 1≤n≤30)
+Line i+1: contains an positive integer k and k positive integers indicating the courses that teacher i can teach (∀i=1,…,m)
+Line m+2: contains an integer k
+Line i+m+2: contains two integer i and j indicating two conflicting courses (∀i=1,…,k)
+Output
+The output contains a unique number which is the maximal load of the teachers in the solution found and the value -1 if not solution found.
+Example
+Input
+4 12
+5 1 3 5 10 12
+5 9 3 4 8 12
+6 1 2 3 4 9 7
+7 1 2 3 5 6 10 11
+25
+1 2
+1 3
+1 5
+2 4
+2 5
+2 6
+3 5
+3 7
+3 10
+4 6
+4 9
+5 6
+5 7
+5 8
+6 8
+6 9
+7 8
+7 10
+7 11
+8 9
+8 11
+8 12
+9 12
+10 11
+11 12
+
+Output
+3
+*/
+
 #include <algorithm>
 #include <climits>
 #include <iostream>
